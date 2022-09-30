a5be3c07c8f2737139a66a7be1c50854a0ee3bc0
Honkitonkie <kooyman.d@gmail.com>
2022-09-30 17:19:34 +0200

    test

--- pages/linkedin/oauth-callback-linkedin.js
+++ pages/linkedin/oauth-callback-linkedin.js
@@ -4,6 +4,8 @@ import { useUser } from "../../lib/hooks";
 
 const Callback = () => {
   const getPath = useRouter();
+
+  console.log("getPath", getPath);
   const [errorMsg, setErrorMsg] = useState("");
 
   if (getPath.query) {
@@ -12,12 +14,10 @@ const Callback = () => {
   async function handleSubmit(e) {
     try {
       const user = await useUser({ redirectTo: "/", redirectIfFound: true })[0];
-
       const body = {
         query: getPath.query,
         email: user?.email,
       };
-
       const res = await fetch("https://automatin.nl/api/linkedin/oauth-callback-linkedin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
