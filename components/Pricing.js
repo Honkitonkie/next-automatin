a9e1713fd3b2d91e3077d6d267d0764789e7a78a
Honkitonkie <kooyman.d@gmail.com>
2022-12-12 15:58:53 +0100

    pricing update

--- components/Pricing.js
+++ components/Pricing.js
@@ -7,6 +7,7 @@ const Pricing = () => {
 
       {/* <!-- Component Start --> */}
       <div className='flex flex-wrap items-center justify-center w-full max-w-4xl mt-8'>
+        {/* <!-- Tile 1 --> */}
         <div className='flex flex-col flex-grow overflow-hidden bg-white rounded-lg shadow-lg hover:scale-105'>
           <div className='flex flex-col items-center p-10 bg-automatin-blue text-white'>
             <span className='font-semibold'>Starter</span>
@@ -67,13 +68,13 @@ const Pricing = () => {
                 <svg className='w-5 h-5 text-green-600 fill-current' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor'>
                   <path fillRule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clipRule='evenodd' />
                 </svg>
-                <span className='ml-2'>Onbeperkt updates</span>
+                <span className='ml-2'>4 templates</span>
               </li>
               <li className='flex items-center'>
                 <svg className='w-5 h-5 text-green-600 fill-current' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor'>
                   <path fillRule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clipRule='evenodd' />
                 </svg>
-                <span className='ml-2'>Onbeperkt templates</span>
+                <span className='ml-2'>4 updates p/m</span>
               </li>
             </ul>
           </div>
@@ -84,6 +85,46 @@ const Pricing = () => {
           </div>
         </div>
         {/* <!-- Tile 3 --> */}
+        <div className='flex flex-col flex-grow overflow-hidden bg-white rounded-lg shadow-lg hover:scale-105'>
+          <div className='flex flex-col items-center p-10 bg-automatin-blue text-white'>
+            <span className='font-semibold'>Expert</span>
+            <div className='flex items-center'>
+              <span className='text-3xl'>€</span>
+              <span className='text-5xl font-bold'>35</span>
+              <span className='text-2xl text-gray-200'>p/m</span>
+            </div>
+          </div>
+          <div className='p-10'>
+            <ul>
+              <li className='flex items-center'>
+                <svg className='w-5 h-5 text-green-600 fill-current' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor'>
+                  <path fillRule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clipRule='evenodd' />
+                </svg>
+                <span className='ml-2 italic'>Custom branding</span>
+              </li>
+              <li className='flex items-center'>
+                <svg className='w-5 h-5 text-green-600 fill-current' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor'>
+                  <path fillRule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clipRule='evenodd' />
+                </svg>
+                <span className='ml-2 '>Onbeperkt templates </span>
+              </li>
+              <li className='flex items-center'>
+                <svg className='w-5 h-5 text-green-600 fill-current' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor'>
+                  <path fillRule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clipRule='evenodd' />
+                </svg>
+                <span className='ml-2'>Onbeperkt updates</span>
+              </li>
+            </ul>
+          </div>
+          <div className='flex px-10 pb-10 justfy-center'>
+            <Link href='/contact'>
+              <button className='flex items-center justify-center w-full h-12 px-6 text-sm uppercase bg-automatin-blue text-white rounded-lg font-semibold hover:bg-blue-400'>Aanmelden</button>
+            </Link>
+          </div>
+        </div>
+
+
+        {/* <!-- Tile 3 met aanbod FB & Insta --> */}
         {/* <div className='flex flex-col flex-grow overflow-hidden bg-white rounded-lg shadow-lg hover:scale-105'>
           <div className='flex flex-col items-center p-10 bg-automatin-blue text-white'>
             <span className='font-semibold'>Expert</span>
